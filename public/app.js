87c7a7d8a39157ed67f28580e2e011feceaafedd
wooden1 <iblackheartyou@gmail.com>
2019-07-05 14:54:02 -0400

    added figcaption and image to home page and styled a bit

--- public/app.js
+++ public/app.js
@@ -1,54 +1,54 @@
 /**
  ** ***************  EATSIT APP ******************
- **           composition by Lynnwood Ray 
- ** **********************************************
- */
+**           composition by Lynnwood Ray
+** **********************************************
+*/
 
- 
-// TODO: Get users zip code to gather list of restuarants near by based on a their distance from user 
+
+// TODO: Get users zip code to gather list of restuarants near by based on a their distance from user
 function ZipCodeFormatException(value) {
-    this.value = value
-    this.message = 'does not conform to the expected format for a zip code'
-    this.toString = function () {
-        return this.value + this.message
-    }
+  this.value = value
+  this.message = 'does not conform to the expected format for a zip code'
+  this.toString = function () {
+    return this.value + this.message
+  }
 }
 
 // Zipcode validation
 function ZipCode(zip) {
-    // zip = new String(zip);
-    const pattern = /[0-9]{5}([- ]?[0-9]{4})?/
-    if (pattern.test(zip)) {
-        // zip code value will be the first match in the string
-        this.value = zip.match(pattern)[0]
-        this.valueOf = function () {
-            return this.value
-        }
-        this.toString = function () {
-            return String(this.value)
-        }
-    } else {
-        throw new ZipCodeFormatException(zip)
+// zip = new String(zip);
+  const pattern = /[0-9]{5}([- ]?[0-9]{4})?/
+  if (pattern.test(zip)) {
+    // zip code value will be the first match in the string
+    this.value = zip.match(pattern)[0]
+    this.valueOf = function () {
+      return this.value
     }
+    this.toString = function () {
+      return String(this.value)
+    }
+  } else {
+    throw new ZipCodeFormatException(zip)
+  }
 }
 
 const zipInput = document.querySelector('input').value
 
 function verifyZipCode(z) {
-    const ZIPCODE_INVALID = -1
-    const ZIPCODE_UNKNOWN_ERROR = -2
+  const ZIPCODE_INVALID = -1
+  const ZIPCODE_UNKNOWN_ERROR = -2
 
-    try {
-        if(z === new ZipCode(zipInput)) {
-            return z
-        } 
-    } catch (e) {
-        if (e instanceof ZipCodeFormatException) {
-            return ZIPCODE_INVALID
-        } 
-            return ZIPCODE_UNKNOWN_ERROR   
+  try {
+    if (z === new ZipCode(zipInput)) {
+      return z
     }
-    return verifyZipCode()
+  } catch (e) {
+    if (e instanceof ZipCodeFormatException) {
+      return ZIPCODE_INVALID
+    }
+    return ZIPCODE_UNKNOWN_ERROR
+  }
+  return verifyZipCode()
 }
 const formElement = document.querySelector('form')
 
@@ -56,21 +56,20 @@ const request = new XMLHttpRequest()
 const display = document.querySelector('display-picks')
 const button = document.querySelector('button')
 
-button.addEventListener('click',() => {
-    const formData = verifyZipCode(zipInput)
-    console.log(formData)
-    // return restuarants
-    request.open('POST', '127.0.0.1:7001', true)
-    request.send(formData)
-    const restaurants = fetch('127.0.0.1:7001/?zip-code=location').then(res => res.json()).catch((err) =>
-        console.error(err))
-    console.log(restaurants)
-    display.innerHTML(restaurants)
-    formElement.zipInput.val('')
-   
+button.addEventListener('click', () => {
+  const formData = verifyZipCode(zipInput)
+  console.log(formData)
+  // return restuarants
+  request.open('POST', '127.0.0.1:7001', true)
+  request.send(formData)
+  const restaurants = fetch('127.0.0.1:7001/?zip-code=location').then(res => res.json()).catch(err =>
+    console.error(err))
+  console.log(restaurants)
+  display.innerHTML(restaurants)
+  formElement.zipInput.val('')
 })
 // TODO: create filtering function that allows user to exclude places (i.e: dietary restrictions, allergies, etc.)
- 
+
 
 // TODO: Create a function for picking a place at random
 
--- server.js
+++ server.js
@@ -1,62 +1,67 @@
+/**
+ ** ***************  EATSIT APP ******************
+**           composition by Lynnwood Ray
+** **********************************************
+*/
+
 require('dotenv').config()
 const path = require('path')
 const yelp = require('yelp-fusion')
 const express = require('express')
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 
 const app = express()
-const urlEncodedParser = bodyParser.urlencoded({extended: false})
+// const urlEncodedParser = bodyParser.urlencoded({ extended: false })
 const apiKey = process.env.YELP_API_KEY
 const client = yelp.client(apiKey)
 const port = 7001
 
 function ZipCodeFormatException(value) {
-    this.value = value
-    this.message = 'does not conform to the expected format for a zip code'
-    this.toString = function () {
-        return this.value + this.message
-    }
+  this.value = value
+  this.message = 'does not conform to the expected format for a zip code'
+  this.toString = function () {
+    return this.value + this.message
+  }
 }
 
 // Zipcode validation
 function ZipCode(zip) {
-    // zip = new String(zip);
-    const pattern = /[0-9]{5}([- ]?[0-9]{4})?/
-    if (pattern.test(zip)) {
-        // zip code value will be the first match in the string
-        this.value = zip.match(pattern)[0]
-        this.valueOf = function () {
-            return this.value
-        }
-        this.toString = function () {
-            return String(this.value)
-        }
-    } else {
-        throw new ZipCodeFormatException(zip)
+// zip = new String(zip);
+  const pattern = /[0-9]{5}([- ]?[0-9]{4})?/
+  if (pattern.test(zip)) {
+    // zip code value will be the first match in the string
+    this.value = zip.match(pattern)[0]
+    this.valueOf = function () {
+      return this.value
     }
+    this.toString = function () {
+      return String(this.value)
+    }
+  } else {
+    throw new ZipCodeFormatException(zip)
+  }
 }
 
 const searchReq = {
-    term: 'restaurants',
-    location: this.location,
-    limit: 5,
+  term: 'restaurants',
+  location: this.location,
+  limit: 5,
 }
 
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/?zip-code=location', (req, res) => {
-    client.search(searchReq).then(response => {
-        const results = JSON.stringify(response.jsonBody.businesses, null, 4)
-        console.log(results)
-        res.send(results)
-        }).catch(e => {
-            console.log(e)
-        })   
-    })
+  client.search(searchReq).then((response) => {
+    const results = JSON.stringify(response.jsonBody.businesses, null, 4)
+    // console.log(results)
+    res.send(results)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
 
-    
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
+  console.log(`Server listening on port ${port}`)
 })
